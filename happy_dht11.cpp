ead8c6651acc5c78edefdaec11eb22080c150d6e
Blake Lloyd <blakelloyd@live.co.uk>
2018-06-16 11:02:25 +0100

    fix(*): Fix issues

--- happy_dht11.cpp
+++ happy_dht11.cpp
@@ -1,59 +1,78 @@
 #include "Arduino.h"
-#include "happy_button.h"
+#include "happy_dht11.h"
 
-class happy_dht11
+float diff = 1.0;
+
+happy_dht11::happy_dht11(int pin, PubSubClient client)
 {
-    float diff = 1.0;
+    _pin = pin;
+    _client = client;
+    DHT dht(pin, DHT11);
+    _dht = &dht;
+    _dht->begin();
+}
 
-    happy_button::happy_dht11(int pin)
+void happy_dht11::setId(char* id)
+{
+    tempConfigTopic = (char*)malloc(strlen(id) + 34 + 1);
+    snprintf(tempConfigTopic, strlen(id) + 34, "homeassistant/sensor/%s-temp/config", id);
+
+    humidConfigTopic = (char*)malloc(strlen(id) + 35 + 1);
+    snprintf(humidConfigTopic, strlen(id) + 35, "homeassistant/sensor/%s-humid/config", id);
+    
+    tempConfig = (char*)malloc(strlen(id) + 48 + 1);
+    snprintf(tempConfig, strlen(id) + 48, "{\"name\": \"%s-temp\", \"unit_of_measurement\": \"°C\"}", id);
+
+    humidConfig = (char*)malloc(strlen(id) + 47 + 1);
+    snprintf(humidConfig, strlen(id) + 47, "{\"name\": \"%s-humid\", \"unit_of_measurement\": \"%%\"}", id);
+    
+    tempStateTopic = (char*)malloc(strlen(id) + 33 + 1);
+    snprintf(tempStateTopic, strlen(id) + 33, "homeassistant/sensor/%s-temp/state", id);
+
+    humidStateTopic = (char*)malloc(strlen(id) + 34 + 1);
+    snprintf(humidStateTopic, strlen(id) + 34, "homeassistant/sensor/%s-humid/state", id);
+    
+    _client.publish(tempConfigTopic, tempConfig, true);
+    _client.publish(humidConfigTopic, humidConfig, true);
+}
+
+bool checkBound(float newValue, float prevValue, float maxDiff) {
+    return !isnan(newValue) &&
+         (newValue < prevValue - maxDiff || newValue > prevValue + maxDiff);
+}
+
+bool happy_dht11::checkTemp()
+{
+    long now = millis();
+    if(now - _lastTempMsg > 1000)
     {
-        _pin = pin;
-        DHT dht(pin, DHT11);
-        dht.begin();
-    }
+        _lastTempMsg = now;
+        float newTemp = _dht->readTemperature();
 
-    bool checkBound(float newValue, float prevValue, float maxDiff) {
-        return !isnan(newValue) &&
-             (newValue < prevValue - maxDiff || newValue > prevValue + maxDiff);
-    }
-
-    bool happy_button::isTempUpdated()
-    {
-        long now = millis();
-        if(now - _lastTempMsg > 1000)
+        if(checkBound(newTemp, _temp, diff))
         {
-            _lastTempMsg = now;
-            float newTemp = dht.readTemperature();
-
-            if(checkBound(newTemp, _temp, diff))
-            {
-                _temp = newTemp;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            _temp = newTemp;
+            _client.publish(tempStateTopic, String(_temp).c_str(), true);
+            return true;
         }
     }
+    return false;
+}
 
-    bool happy_button::isHumidUpdated()
+bool happy_dht11::checkHumid()
+{
+    long now = millis();
+    if(now - _lastHumidMsg > 1000)
     {
-        long now = millis();
-        if(now - _lastHumidMsg > 1000)
-        {
-            _lastHumidMsg = now;
-            float newHumid = dht.readHumidity();
+        _lastHumidMsg = now;
+        float newHumid = _dht->readHumidity();
 
-            if(checkBound(newHumid, _humid, diff))
-            {
-                _humid = newHumid;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+        if(checkBound(newHumid, _humid, diff))
+        {
+            _humid = newHumid;
+            _client.publish(humidStateTopic, String(_humid).c_str(), true);
+            return true;
         }
     }
+    return false;
 }
--- happy_dht11.h
+++ happy_dht11.h
@@ -1,23 +1,31 @@
-#ifndef happy_button_h
-#define happy_button_h
+#ifndef happy_dht11_h
+#define happy_dht11_h
 
 #include "Arduino.h"
 #include "DHT.h"
+#include <PubSubClient.h>
 
 class happy_dht11
 {
     public:
-        happy_dht11(int pin);
-        bool isTempUpdated();
-        bool isHumidUpdated();
-        float getTemp();
-        float getHumid();
+        happy_dht11(int pin, PubSubClient client);
+        void setId(char* id);
+        bool checkTemp();
+        bool checkHumid();
     private:
         int _pin;
         long _lastTempMsg;
         long _lastHumidMsg;
         float _temp;
         float _humid;
-}
+        PubSubClient _client;
+        char *tempConfigTopic;
+        char *humidConfigTopic;
+        char *tempStateTopic;
+        char *humidStateTopic;
+        char *tempConfig;
+        char *humidConfig;
+        DHT *_dht;
+};
 
 #endif
